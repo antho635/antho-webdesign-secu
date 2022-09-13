77311b065dfd29a0f3f7cbf0986598350cb83ef9
Toutchy kipik <toutchy.kipik@pm.me>
2022-09-14 00:36:29 +0200

    blog ok

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index_blog, PostDetail, PostList
 
 urlpatterns = [
-    path('blog/index/', index_blog, name="index_blog"),
-    path('blog/', PostList.as_view(), name='list_post'),
+    path('blog/', index_blog, name="index_blog"),
+    path('blog/list', PostList.as_view(), name='list_post'),
     path('blog/post_detail/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
