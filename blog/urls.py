1c96a549026179c2f70326ad22c8df6165fa4b8b
Toutchy kipik <toutchy.kipik@pm.me>
2022-09-24 23:24:40 +0200

    db  oksss

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import PostDetail, PostList, index_test, index_blog, dev_actus, hack_actus, cryptos_actus, ia_actus, \
+
+from .views import PostDetail, PostList, index_test, index_blog, \
     DevActus, DevActusDetail, HackActus, CryptosActus, IaActus
 
 urlpatterns = [
