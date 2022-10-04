5f13253db1d522fef5a7f36021cfcd3e30fc2f25
Toutchy kipik <toutchy.kipik@pm.me>
2022-10-04 21:49:57 +0200

    project mofrld

--- portfolio/views.py
+++ portfolio/views.py
@@ -17,5 +17,21 @@ def pizzeria(request):
 
 
 def project_list(request):
-
     return render(request, 'portfolio/projets/project_list.html')
+
+# affichr les projets
+# class ProjectListView(generic.ListView):
+#     queryset = Project.objects.filter(status=1).order_by('-created_on')
+#     template_name = 'portfolio/projets/project_list.html'
+
+# afficher les details d'un projet
+# class ProjectListViewDetails(generic.DetailView):
+#     model = Project
+#     template_name = 'portfolio/projets/project_details.html'
+
+# def project_list(request):
+#     return render(request, 'portfolio/projets/project_list.html')
+
+# def project_details(request):
+#     return render(request, 'portfolio/projets/project_details.html')
+
