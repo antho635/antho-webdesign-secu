61618e41ec22b88fefccf181b62bff40560aa60c
Toutchy kipik <toutchy.kipik@pm.me>
2022-11-11 02:28:15 +0100

    status title ok

--- blog/views.py
+++ blog/views.py
@@ -13,11 +13,6 @@ def index_test(request):
     return render(request, 'blog/index_test.html')
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog/list_post.html'
-
-
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
