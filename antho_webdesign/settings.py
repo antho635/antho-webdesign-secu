be197bb330dfdaf6c24b6433383ccab55d810a6c
Toutchy kipik <toutchy.kipik@pm.me.Toutchy kipik>
2022-11-27 00:29:39 +0100

    responsive index blog + portfolio

--- antho_webdesign/settings.py
+++ antho_webdesign/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import dj_database_url
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,4 +126,4 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 SASS_COMPILER_MAP = True
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
